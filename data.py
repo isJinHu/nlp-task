931107534ec5191e1a5b243a30f80df83f25bbc5
Jin Hu <49333550+isJinHu@users.noreply.github.com>
2023-01-15 23:55:48 +0800

    Update data.py

--- data.py
+++ data.py
@@ -6,8 +6,6 @@ import numpy as np
 
 class LanguageModelDataset(Dataset):
     def __init__(self, data, max_seq_len):
-        # self.dataset = load_dataset('text', data_files={'train': args.train_path, 'dev': args.dev_path})
-        # convert_exp(dataset['train'], tokenizer=None, max_seq_len=args.max_seq_len)
         self.vocab = get_vocab()
         self.vocab_size = len(self.vocab)
         self.lines = []
@@ -16,7 +14,7 @@ class LanguageModelDataset(Dataset):
         self.max_seq_len = max_seq_len
 
     def __getitem__(self, index):
-        tokenized_output = {
+        item = {
             'input_ids': None,
             'labels': None
         }
@@ -29,12 +27,12 @@ class LanguageModelDataset(Dataset):
         if len(input_ids) < self.max_seq_len:
             input_ids = input_ids + [0 for _ in range(self.max_seq_len - len(input_ids))]
 
-        tokenized_output['input_ids']=input_ids
-        tokenized_output['labels']=int(label)
+        item['input_ids']=input_ids
+        item['labels']=int(label)
 
-        for k, v in tokenized_output.items():
-            tokenized_output[k] = np.array(v)
-        return tokenized_output
+        for k, v in item.items():
+            item[k] = np.array(v)
+        return item
 
     def __len__(self):
         return len(self.lines)
@@ -54,7 +52,7 @@ def get_vocab():
         word_list += [w for w in content]
     word_list = list(set(word_list))
     word_dict = {w: i for i, w in enumerate(word_list)}
-    print(len(word_list))
+    # print(len(word_list))
     return word_dict
 
 
